92fb3069b3fc3e74e0fdc628ca4fddc455c4f46c
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-18 02:34:43 -0500

    Update tutorial.service.js

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,32 +1,41 @@
-import http from './http-common';
-
+import axios from "axios";
+ 
+const http = axios.create({
+  baseURL: "http://192.168.50.3:8080/api",
+  /*baseURL: "http://localhost:8080/api",*/
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+ 
 const getAll = () => {
-  return http.get('/tutorials');
+  return http.get("/tutorials");
 };
+ 
 const get = (id) => {
-  return http.get(`/tutorials/\${id}`);
+  return http.get(`/tutorials/${id}`);
 };
-
+ 
 const create = (data) => {
-  return http.post('/tutorials', data);
+  return http.post("/tutorials", data);
 };
-
+ 
 const update = (id, data) => {
-  return http.put(`/tutorials/\${id}`, data);
+  return http.put(`/tutorials/${id}`, data);
 };
-
+ 
 const remove = (id) => {
-  return http.delete(`/tutorials/\${id}`);
+  return http.delete(`/tutorials/${id}`);
 };
-
+ 
 const removeAll = () => {
-  return http.delete('/tutorials');
+  return http.delete("/tutorials");
 };
-
+ 
 const findByTitle = (title) => {
-  return http.get(`/tutorials?title=\${title}`);
+  return http.get(`/tutorials?title=${title}`);
 };
-
+ 
 export default {
   getAll,
   get,
