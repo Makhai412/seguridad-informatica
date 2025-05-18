a5289d1917bb97190e544415039d0d1c4ee83e5c
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-18 00:15:08 -0500

    Update tutorial.service.js

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,12 +1,3 @@
-import axios from "axios";
- 
-const http = axios.create({
-  baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
- 
 const getAll = () => {
   return http.get("/tutorials");
 };
