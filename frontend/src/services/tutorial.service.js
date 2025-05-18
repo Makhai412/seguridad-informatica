5f37fc8b7a53f1271c9c2bf08371d654d917dfb8
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-18 01:31:48 -0500

    Update tutorial.service.js

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,31 +1,32 @@
+import http from './http-common';
+
 const getAll = () => {
-  return http.get("/tutorials");
+  return http.get('/tutorials');
 };
- 
 const get = (id) => {
-  return http.get(`/tutorials/${id}`);
+  return http.get(`/tutorials/\${id}`);
 };
- 
+
 const create = (data) => {
-  return http.post("/tutorials", data);
+  return http.post('/tutorials', data);
 };
- 
+
 const update = (id, data) => {
-  return http.put(`/tutorials/${id}`, data);
+  return http.put(`/tutorials/\${id}`, data);
 };
- 
+
 const remove = (id) => {
-  return http.delete(`/tutorials/${id}`);
+  return http.delete(`/tutorials/\${id}`);
 };
- 
+
 const removeAll = () => {
-  return http.delete("/tutorials");
+  return http.delete('/tutorials');
 };
- 
+
 const findByTitle = (title) => {
-  return http.get(`/tutorials?title=${title}`);
+  return http.get(`/tutorials?title=\${title}`);
 };
- 
+
 export default {
   getAll,
   get,
