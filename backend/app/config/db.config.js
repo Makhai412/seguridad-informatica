aa28a3e844074b40c7e5bacd10fb587e85030dae
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-18 23:57:03 -0500

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,14 +1,15 @@
+
 export default {
-    HOST: 'localhost',
-    USER: 'vagrant',
-    PASSWORD: 'vagrant',
-    DB: 'todoapp',
-    dialect: 'postgres',
-    PORT: 5432,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: process.env.DB_HOST || '192.168.50.20',
+  USER: process.env.DB_USER || 'vagrant',
+  PASSWORD: process.env.DB_PASSWORD || 'vagrant',
+  DB: process.env.DB_NAME || 'todoapp',
+  dialect: 'postgres',
+  PORT: process.env.DB_PORT || 5432,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
