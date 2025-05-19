d39b06bc155ad6fe0cf1f5d223e172a2622f253a
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-18 23:57:38 -0500

    Update tutorial.routes.js

--- backend/app/routes/tutorial.routes.js
+++ backend/app/routes/tutorial.routes.js
@@ -1,29 +1,29 @@
 import * as tutorials from "../controllers/tutorial.controller.js";
 import express from "express";
- 
+
 export default (app) => {
   let router = express.Router();
- 
+  
   // Create a new Tutorial
   router.post("/", tutorials.create);
- 
+  
   // Retrieve all Tutorials
   router.get("/", tutorials.findAll);
- 
+  
+  // Find all published Tutorials (IMPORTANTE: esta ruta debe ir antes de /:id)
+  router.get("/published", tutorials.findAllPublished);
+  
   // Retrieve a single Tutorial with id
   router.get("/:id", tutorials.findOne);
- 
+  
   // Update a Tutorial with id
   router.put("/:id", tutorials.update);
- 
+  
   // Delete a Tutorial with id
   router.delete("/:id", tutorials.deleteOne);
- 
+  
   // Delete all Tutorials
   router.delete("/", tutorials.deleteAll);
- 
-  // Find all published Tutorials
-  router.get("/published", tutorials.findAllPublished);
- 
+  
   app.use('/api/tutorials', router);
 };
